b8e7decb30c99e68e02f07863ff3cdcfbf6a12b1
Thomas Barrett <tsbarrett89@gmail.com>
2019-12-13 10:43:48 -0700

    finished table shcema

---
+++ data/migrations/20191213092457_schemas.js
@@ -0,0 +1,54 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  
+  .createTable('projects', tbl => {
+      tbl.increments('id');
+      tbl.string('name').notNullable();
+      tbl.string('description');
+      tbl.boolean('complete').defaultTo(false);
+  })
+
+  .createTable('resources', tbl => {
+      tbl.increments('id');
+      tbl.string('name').notNullable();
+      tbl.string('description');
+  })
+
+  .creatTable('tasks', tbl => {
+      tbl.increments('id');
+      tbl.string('description').notNullable();
+      tbl.string('notes');
+      tbl.boolean('complete').defaultTo(false);
+      tbl.integer('project_id')
+      .unsigned()
+      .references('id')
+      .inTable('projects')
+      .onDelete('RESTRICT') 
+      .onUpdate('CASCADE');
+  })
+
+  .creatTable('project_resources', tbl => {
+      tbl.increments();
+      tbl.integer('resource_id')
+        .unsigned()
+        .references('id')
+        .inTable('resources')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+      tbl.integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+            .dropTableIfExists('project_resources')
+            .dropTableIfExists('tasks')
+            .dropTableIfExists('resources')
+            .dropTableIfExists('projects')
+};
